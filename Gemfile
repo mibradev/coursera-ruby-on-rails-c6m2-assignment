4f905d9a4421c2ce2a3c0eaac3df08a4aea042a9
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-02-21 19:47:57 +0200

    Configure databases

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.3.8'
 gem 'rails', '4.2.11'
 gem 'rails-api', '~> 0.4.1'
 gem 'pg', '~> 0.21'
+gem 'mongoid', '~> 5.4.0'
 
 group :development do
   gem 'spring', '~> 2.0.2'
--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module CourseraRubyOnRailsC6m1Assignment
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    Mongoid.load!("./config/mongoid.yml")
+    config.generators { |g| g.orm :active_record }
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
